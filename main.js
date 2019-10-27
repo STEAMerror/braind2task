c29fbb76032f50c8356bee2734de88b335967985
STEAMerror <55356151+STEAMerror@users.noreply.github.com>
2019-10-27 23:12:13 +0300

    add srollLock

--- main.js
+++ main.js
@@ -6,19 +6,30 @@ var btn = document.getElementById("myBtn");
 
 var span = document.getElementsByClassName("close")[0];
 
-
+//Открывает модальное окно
 btn.onclick = function() {
     modal.style.display = "block";
+    disableScrolling();
 }
 
-
+//Закрывает модальное окно
 span.onclick = function() {
     modal.style.display = "none";
+    enableScrolling(); //Вызов функции, разрешающей прокрутку
 }
-
-
+//Закрывает окно при нажатии на фон
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+//Блокирует прокрутку страницы
+function disableScrolling(){
+    var x=window.scrollX;
+    var y=window.scrollY;
+    window.onscroll=function(){window.scrollTo(x, y);}; 
+}
+//Разрешает прокрутку страницы
+function enableScrolling(){
+    window.onscroll=function(){};
 }
