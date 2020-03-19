3afad4ca60cbea01e4fb2daa3af2b709aea57188
STEAMerror <dimbooss@mail.ru>
2020-03-19 16:57:02 +0300

    reconstruction

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 var modal = document.getElementById('myModal');
-
-
+var body = document.getElementById('modal_body');
 var btn = document.getElementById("myBtn");
-
-
 var span = document.getElementsByClassName("close")[0];
-
 //Открывает модальное окно
 btn.onclick = function() {
     modal.style.display = "block";
     disableScrolling();
+	body.style.overflow = "hidden";
 }
-
 //Закрывает модальное окно
 span.onclick = function() {
     modal.style.display = "none";
     enableScrolling(); //Вызов функции, разрешающей прокрутку
+	body.style.overflow = "auto";
 }
 //Закрывает окно при нажатии на фон
 window.onclick = function(event) {
